12a31333239daedb707a8d558184a58a22cff812
Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>
2023-10-21 13:35:09 +0530

    Removed the system links

--- batch-rm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# get all the other arguments passed to this script so that they can be passed to rm
-
-ARGS=("$@")
-
-while read -r FILE; do
-    rm "$FILE" "${ARGS[@]}" && echo "$FILE"
-done
